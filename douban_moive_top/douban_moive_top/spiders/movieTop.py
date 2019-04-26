8185b02984dad41197cc84179ff02ece09439785
kenja <yagamelai@hotmail.com>
2019-04-27 05:51:42 +0800

    引入 items 的类, 修改代码使数据保存到 items 中

--- douban_moive_top/douban_moive_top/spiders/movieTop.py
+++ douban_moive_top/douban_moive_top/spiders/movieTop.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from douban_moive_top.items import DoubanMoiveTopItem
 
 class MovietopSpider(scrapy.Spider):
     name = 'movieTop'
@@ -8,16 +8,17 @@ class MovietopSpider(scrapy.Spider):
     start_urls = ['https://movie.douban.com/top250']
 
     def parse(self, response):
-        items = response.xpath('//li')
-        for item in items:
+        items = DoubanMoiveTopItem()
+        movies = response.xpath('//ol/li')
+        print(str(len(movies)) + '个结果')
+        for each in movies:
             # 目标是抓取 5 项内容: 标题 图片 详情链接 评分 短评
-            yield {
-                'title': item.xpath('div[@class="item"]/div[@class="info"]//a/span[1]/text()').extract_first(),
-                'link': item.xpath('div[@class="item"]/div[@class="info"]//a/@href').extract_first(),
-                'image': item.xpath('div[@class="item"]/div[@class="pic"]/a/img/@src').extract_first(),
-                'rating_num': item.xpath('div[@class="item"]//span[@class="rating_num"]/text()').extract_first(),
-                'quote': item.xpath('div[@class="item"]//p[@class="quote"]/span/text()').extract_first(),
-            }
+            items['title'] = each.xpath('div[@class="item"]/div[@class="info"]//a/span[1]/text()').extract_first()
+            items['link'] = each.xpath('div[@class="item"]/div[@class="info"]//a/@href').extract_first()
+            items['image'] = each.xpath('div[@class="item"]/div[@class="pic"]/a/img/@src').extract_first()
+            items['rating_num'] = each.xpath('div[@class="item"]//span[@class="rating_num"]/text()').extract_first()
+            items['quote'] = each.xpath('div[@class="item"]//p[@class="quote"]/span/text()').extract_first()
+            yield items
 
         next_page = response.xpath('//*[@id="content"]/div/div[1]/div[2]/span[3]/a/@href').extract_first()
         # 如果每个页面都有下一页的话就执行 if 代码块.
