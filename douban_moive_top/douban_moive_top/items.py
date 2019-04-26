226df450f9e390dbdd3553d41cc2aa3fa1869551
kenja <yagamelai@hotmail.com>
2019-04-27 05:43:28 +0800

    通过 items.py 把数据转化成固定结构, 以便保存和展示

--- douban_moive_top/douban_moive_top/items.py
+++ douban_moive_top/douban_moive_top/items.py
@@ -11,4 +11,8 @@ import scrapy
 class DoubanMoiveTopItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    title = scrapy.Field()
+    link = scrapy.Field()
+    image = scrapy.Field()
+    rating_num = scrapy.Field()
+    quote = scrapy.Field()
